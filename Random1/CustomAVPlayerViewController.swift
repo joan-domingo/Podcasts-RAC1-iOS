b8b7156c2a2cd6d86b4c402627f58ee27dac2a95
Joan Domingo <joands@gmail.com>
2017-04-30 17:08:27 +0200

    code cleanup

--- Random1/CustomAVPlayerViewController.swift
+++ Random1/CustomAVPlayerViewController.swift
@@ -26,24 +26,5 @@ class CustomAVPlayerViewController: AVPlayerViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let albumArt = UIImageView()
-        //albumArt.kf.setImage(with: imageUrl!)
-        self.contentOverlayView?.addSubview(albumArt)
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
